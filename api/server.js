943d40abb5a6135cf32498df277170d0f7cf21f3
Tony Sorensen <tony48853@gmail.com>
2021-05-10 15:52:37 -0700

    all routes functional

--- api/server.js
+++ api/server.js
@@ -7,6 +7,6 @@ const server = express();
 
 server.use(express.json());
 
-server.use("/api/posts/posts-router.js", postsRouter);
+server.use("/api/posts/", postsRouter);
 
 module.exports = server;
